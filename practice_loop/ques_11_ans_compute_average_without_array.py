a49d8770c4eea9c750779f554ed7e2225acd0959
ANAHAN07 <ahantowsee.nasifa@gmail.com>
2025-01-31 02:13:03 +0600

    some tab changes

--- practice_loop/ques_11_ans_compute_average_without_array.py
+++ practice_loop/ques_11_ans_compute_average_without_array.py
@@ -4,10 +4,10 @@ for _ in range(y):
     total += int(input("Enter number: ")) 
     # Output: 
 """ 
-Enter number: 69
-Enter number: 90
-Enter number: 50
-Enter number: 38 
+    Enter number: 69
+    Enter number: 90
+    Enter number: 50
+    Enter number: 38 
 """
     
 print("Average:", total / y)        # Output: Average: 61.75
